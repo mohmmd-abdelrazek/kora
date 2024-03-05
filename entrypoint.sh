3bb52db857c5b5dc5c85c02507eed173d3363e81
Mohamed <mohmmd.abdelrazek@gmail.com>
2024-03-05 09:44:41 +0200

    remove wait for function

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,23 +4,23 @@
 cd /app/frontend
 npm run start &
 # Wait for the backend service to start
-wait_for_service() {
-  SERVICE_HOST=$1
-  SERVICE_PORT=$2
-  echo "Waiting for ${SERVICE_HOST}:${SERVICE_PORT} to be ready..."
-  while ! nc -z ${SERVICE_HOST} ${SERVICE_PORT}; do
-    sleep 1
-  done
-  echo "${SERVICE_HOST}:${SERVICE_PORT} is up and running."
-}
-wait_for_service localhost 3000
+# wait_for_service() {
+#   SERVICE_HOST=$1
+#   SERVICE_PORT=$2
+#   echo "Waiting for ${SERVICE_HOST}:${SERVICE_PORT} to be ready..."
+#   while ! nc -z ${SERVICE_HOST} ${SERVICE_PORT}; do
+#     sleep 1
+#   done
+#   echo "${SERVICE_HOST}:${SERVICE_PORT} is up and running."
+# }
+# wait_for_service localhost 3000
 
-# Start the Next.js application
-cd /app/backend
-npm run start &
+# # Start the Next.js application
+# cd /app/backend
+# npm run start &
 
-# Wait for the frontend service to start
-wait_for_service localhost 5000
+# # Wait for the frontend service to start
+# wait_for_service localhost 5000
 
 # Start Nginx in the foreground
 nginx -g 'daemon off;'
