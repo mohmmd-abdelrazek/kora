d908d275cfd00780153d032c65b3176bffb0ddb3
Mohamed <mohmmd.abdelrazek@gmail.com>
2024-03-05 13:25:38 +0200

    put wait function for entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,26 +1,28 @@
 #!/bin/sh
 
-# Start the Express.js application
-cd /app/frontend
-npm run start &
-# Wait for the backend service to start
-# wait_for_service() {
-#   SERVICE_HOST=$1
-#   SERVICE_PORT=$2
-#   echo "Waiting for ${SERVICE_HOST}:${SERVICE_PORT} to be ready..."
-#   while ! nc -z ${SERVICE_HOST} ${SERVICE_PORT}; do
-#     sleep 1
-#   done
-#   echo "${SERVICE_HOST}:${SERVICE_PORT} is up and running."
-# }
-# wait_for_service localhost 3000
+# Function to check if a service is ready
+wait_for_service() {
+  SERVICE_HOST=$1
+  SERVICE_PORT=$2
+  echo "Waiting for ${SERVICE_HOST}:${SERVICE_PORT} to be ready..."
+  while ! nc -z ${SERVICE_HOST} ${SERVICE_PORT}; do
+    sleep 1
+  done
+  echo "${SERVICE_HOST}:${SERVICE_PORT} is up and running."
+}
 
-# Start the Next.js application
+# Start the Express.js (backend) application
+echo "Starting backend service..."
 cd /app/backend
 npm run start &
+wait_for_service localhost 5000
 
-# Wait for the frontend service to start
-# wait_for_service localhost 5000
+# Start the Next.js (frontend) application
+echo "Starting frontend service..."
+cd /app/frontend
+npm run start &
+wait_for_service localhost 3000
 
 # Start Nginx in the foreground
+echo "Starting Nginx..."
 nginx -g 'daemon off;'
