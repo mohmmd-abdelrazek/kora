fdd864285abc4a73d05223d1ddc73ad92a95803a
Mohamed <mohmmd.abdelrazek@gmail.com>
2024-03-06 16:48:07 +0200

    optimizing the entrypoint script

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,28 +4,27 @@ wait_for_service() {
   SERVICE_PORT=$2
   echo "Waiting for ${SERVICE_HOST}:${SERVICE_PORT} to be ready..."
   while ! nc -z ${SERVICE_HOST} ${SERVICE_PORT}; do
-    sleep 1
+    sleep 0.5
   done
   echo "${SERVICE_HOST}:${SERVICE_PORT} is up and running."
 }
 
 echo "Starting Nginx with the initial configuration..."
-nginx -g 'daemon off;' &
+nginx
 
 echo "Starting frontend service..."
 cd /app/frontend
 npm run start &
-wait_for_service localhost 3000
 
 echo "Starting backend service..."
 cd /app/backend
 npm run start &
+
+wait_for_service localhost 3000
 wait_for_service localhost 5000
 
-echo "Updating Nginx configuration to the final setup..."
-cp /etc/nginx/final.conf /etc/nginx/nginx.conf
 echo "Reloading Nginx..."
+cp /etc/nginx/final.conf /etc/nginx/nginx.conf
 nginx -s reload
 
 echo "All services are up and running."
-wait $!
