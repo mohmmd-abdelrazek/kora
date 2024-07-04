8a44fbf9a227164ded14fb4180e14bc1fbc77009
Mohamed <mohmmd.abdelrazek@gmail.com>
2024-07-05 00:57:03 +0300

    fix certificate path

--- backend/src/config/pgConfig.ts
+++ backend/src/config/pgConfig.ts
@@ -1,6 +1,5 @@
 import { Pool } from "pg";
 import fs from "fs";
-import path from "path";
 
 const connectionString = process.env.DATABASE_URL;
 
@@ -8,12 +7,13 @@ if (!connectionString) {
   throw new Error("DATABASE_URL is not defined");
 }
 
-const sslConfig = process.env.EXPRESS_ENV === "production" 
-  ? {
-      ca: fs.readFileSync(path.resolve(__dirname, "./ca.pem")),
-      rejectUnauthorized: true
-    }
-  : false;
+const sslConfig =
+  process.env.EXPRESS_ENV === "production"
+    ? {
+        ca: fs.readFileSync("./ca.pem"),
+        rejectUnauthorized: true,
+      }
+    : false;
 
 const pool = new Pool({
   connectionString: connectionString,
