fab14bfe3f34efb4a4dcf8eb431a7830d037bc2a
Mohamed <mohmmd.abdelrazek@gmail.com>
2024-07-05 00:42:50 +0300

    add ssl certificate

--- backend/src/config/pgConfig.ts
+++ backend/src/config/pgConfig.ts
@@ -1,4 +1,6 @@
 import { Pool } from "pg";
+import fs from "fs";
+import path from "path";
 
 const connectionString = process.env.DATABASE_URL;
 
@@ -6,9 +8,16 @@ if (!connectionString) {
   throw new Error("DATABASE_URL is not defined");
 }
 
+const sslConfig = process.env.EXPRESS_ENV === "production" 
+  ? {
+      ca: fs.readFileSync(path.resolve(__dirname, "./ca.pem")),
+      rejectUnauthorized: true
+    }
+  : false;
+
 const pool = new Pool({
   connectionString: connectionString,
-  ssl: { rejectUnauthorized: process.env.EXPRESS_ENV === "production" },
+  ssl: sslConfig,
 });
 
 export default pool;
