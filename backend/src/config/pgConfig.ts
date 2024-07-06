23ceaf3a1469ba98383f94b6837ced5852637147
Mohamed <mohmmd.abdelrazek@gmail.com>
2024-07-06 15:12:32 +0300

    configure now postgres database

--- backend/src/config/pgConfig.ts
+++ backend/src/config/pgConfig.ts
@@ -1,5 +1,6 @@
 import { Pool } from "pg";
 import fs from "fs";
+import path from "path";
 
 const connectionString = process.env.DATABASE_URL;
 
@@ -7,17 +8,15 @@ if (!connectionString) {
   throw new Error("DATABASE_URL is not defined");
 }
 
-const sslConfig =
-  process.env.EXPRESS_ENV === "production"
-    ? {
-        ca: fs.readFileSync("./ca.pem"),
-        rejectUnauthorized: true,
-      }
-    : false;
+// Use path.resolve to construct an absolute path to ca.pem
+const caCertPath = path.resolve(__dirname, "./ca.pem");
 
 const pool = new Pool({
   connectionString: connectionString,
-  ssl: sslConfig,
+  ssl: { 
+    ca: fs.readFileSync(caCertPath),
+    rejectUnauthorized: process.env.EXPRESS_ENV === "production"
+  },
 });
 
 export default pool;
